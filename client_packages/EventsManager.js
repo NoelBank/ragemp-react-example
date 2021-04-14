916008f0a7f8b3acf1e018f7aaeec9f6d38f895a
Noel Bank <bank@neoskop.de>
2021-04-14 14:12:53 +0200

    add ugly eventmanager

---
+++ client_packages/EventsManager.js
@@ -0,0 +1,28 @@
+var EventManager = {
+  events: {},
+  on: function (eventName, handler) {
+    if (eventName in this.events) {
+      this.events[eventName].push(handler);
+    } else {
+      this.events[eventName] = [handler];
+    }
+  },
+  removeHandler: function (eventName, handler) {
+    if (eventName in this.events) {
+      var index = this.events[eventName].indexOf(handler);
+      this.events[eventName].splice(index, 1);
+    }
+  },
+};
+
+export function trigger(eventName, args) {
+  var handlers = EventManager.events[eventName];
+  try {
+    var data = JSON.parse(args);
+  } catch (e) {
+    mp.trigger("uiException", e.message);
+  }
+  handlers.forEach((handler) => handler(data));
+}
+
+export default EventManager;
